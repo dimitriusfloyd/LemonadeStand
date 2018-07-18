4f2d5a15a8094bfdcf1b83ffef7014aac84df4b7
Dimitrius Floyd <dimitriusfloyd@gmail.com>
2018-07-18 10:52:33 -0500

    new stuff

--- LemonadeStand/Lemonade.cs
+++ LemonadeStand/Lemonade.cs
@@ -6,5 +6,19 @@ namespace LemonadeStand
 {
     class Lemonade
     {
+        public string NameOfStand { get; set; }
+        public int NumberOfCups { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public decimal Revenue (decimal Price, int NumberOfCups)
+        {
+            return Price * NumberOfCups;
+        }
+        public decimal Profit (decimal Price, decimal Cost)
+        {
+
+            return Price - Cost;
+        }
+
     }
 }
--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -6,7 +6,27 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Lemonade myStand = new Lemonade();
+            myStand.NameOfStand = "Good Lemonade";
+            myStand.NumberOfCups = 100;
+            myStand.Price = 1;
+            myStand.Cost = .25;
+
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("What will the name of your lemonade stand be?");
+            string NameOfStand = Console.ReadLine();
+
+            Console.WriteLine("Thanks!");
+            Console.WriteLine("Your stand will be called " + NameOfStand);
+
+            Console.WriteLine("How many cups will you sell:");
+            int NumberOfCups = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Thanks!");
+            Console.WriteLine("How much will you sell each cup for?");
+
+            decimal Price = Int32.Parse(Console.ReadLine());
+            decimal Revenue = NumberOfCups * 
         }
     }
-}
+
